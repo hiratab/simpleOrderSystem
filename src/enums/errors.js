d53613db18f462a9a8cf2847ddbccf8c24f7f1a2
Bruno Hirata <brn.hirata@gmail.com>
2021-08-26 21:37:59 -0300

    Add improvements on error code

---
+++ src/enums/errors.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    USER_NOT_FOUND : 'ERROR_0001',
+    USER_ID_NOT_INCLUDED : 'ERROR_0002'
+})
--- src/service/usersService.js
+++ src/service/usersService.js
@@ -2,15 +2,22 @@
 
 const { getUserDB } = require('../repository/userRepository')
 
+const Error = require('../enums/errors')
+
 const getUser = async (req, res, next) => {
     const { userId } = req.body
     if(!userId) {
-        return res.status(400).send()
+        return res.status(400).json({
+            code: Error.USER_ID_NOT_INCLUDED
+        })
     }
 
     const user = await getUserDB(userId)
     if(!user) {
-        return res.status(400).send()
+        console.log()
+        return res.status(400).json({
+            code: Error.USER_NOT_FOUND
+        })
     }
 
     req.user = user
